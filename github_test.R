246cb4a24bfab592fc0a4936a39c3453d8c984f0
adammo2904 <adammo2904@gmail.com>
2023-05-30 09:39:21 +0200

    Penguins :)

--- github_test.R
+++ github_test.R
@@ -4,3 +4,22 @@ git_default_branch_configure(name = "main")
 git_vaccinate()
 use_git()
 use_github()
+
+#Adding some data
+library(tidyverse)
+library(ggplot2)
+penguins <- palmerpenguins::penguins
+
+plot <- ggplot(data = penguins, aes(x = species,
+                   y = body_mass_g
+                     )) +
+  geom_violin() +
+  geom_jitter(size = 1.5,
+              aes(fill = island),
+              pch = 21) +
+  theme_bw() +
+  facet_wrap(~ island) +
+  labs(title = "Penguin weight across habitats",
+       x = "Species",
+       y = "Body mass")
+plot
